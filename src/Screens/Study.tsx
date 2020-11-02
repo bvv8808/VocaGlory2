5dde038d0a8fa5fa3b61b7a6c043817503d17ff7
hyeonvvoo <hyeonwoo.v24@gmail.com>
2020-11-03 01:30:12 +0900

    #1102 making study screen

--- src/Screens/Study.tsx
+++ src/Screens/Study.tsx
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ToastAndroid,
+} from 'react-native';
+import CustomHeader from '~/components/customHeader';
 import axios from 'axios';
 
 interface prop {
@@ -9,7 +16,9 @@ interface prop {
 
 const StudyScreen = ({navigation, route}: prop) => {
   const [words, setWords] = useState(new Array());
-  const [wordsLen, setWordsLen] = useState(-1);
+  const [curWord, setCurWord] = useState({voca: '', pronounce: '', mean: ''});
+  const [curIdx, setCurIdx] = useState(-1);
+  const [shownMean, setShownMean] = useState(false);
 
   useEffect(() => {
     axios
@@ -21,28 +30,55 @@ const StudyScreen = ({navigation, route}: prop) => {
           else return 1;
         });
         setWords(sortedWords);
+        // setCurVoca(sortedWords[0].voca);
+        // setCurPronounce(sortedWords[0].procounce);
+        // setCurMean(sortedWords[0].mean);
+        setCurWord(sortedWords[0]);
+        setCurIdx(0);
       });
   }, []);
 
   return (
     <View style={s.wrap}>
-      <Text>{route.params.title}</Text>
-      <TouchableOpacity style={s.wordContainer}></TouchableOpacity>
-      {/* <View style={s.btnContainer}>
+      <CustomHeader title="Study" />
+      <TouchableOpacity
+        style={s.wordContainer}
+        onPressOut={() => {
+          if (shownMean === false) {
+            setShownMean(true);
+          } else {
+            if (curIdx !== -1 && curIdx <= words.length - 1) {
+              if (curIdx === words.length - 1) {
+                ToastAndroid.show(
+                  '해당 어원의 단어 공부가 끝났습니다',
+                  ToastAndroid.SHORT,
+                );
+                navigation.goBack();
+              } else {
+                setShownMean(false);
+
+                const nextIdx = curIdx + 1;
+                setCurWord(words[nextIdx]);
+                setCurIdx(nextIdx);
+              }
+            }
+          }
+        }}>
+        <Text style={s.txtVoca}>
+          {curIdx < words.length ? curWord.voca : ''}
+        </Text>
+        <Text style={s.txtPronounce}>
+          {curIdx < words.length ? curWord.pronounce : ''}
+        </Text>
+        <Text style={s.txtMean}>
+          {curIdx < words.length && shownMean ? curWord.mean : ''}
+        </Text>
+      </TouchableOpacity>
+
+      <View style={s.btnContainer}>
         <TouchableOpacity style={s.btnAdd}>
           <Text style={s.txtAdd}>단어장에 추가</Text>
         </TouchableOpacity>
-      </View> */}
-      <View>
-        {words.map((word: any, idx: number) => {
-          return (
-            <View key={idx} style={{padding: 10, borderWidth: 1}}>
-              <Text>{word.voca}</Text>
-              <Text style={{fontFamily: 'wdpron'}}>{word.pronounce}</Text>
-              <Text>{word.mean}</Text>
-            </View>
-          );
-        })}
       </View>
     </View>
   );
@@ -52,8 +88,30 @@ const s = StyleSheet.create({
   wrap: {
     flex: 1,
   },
-  wordContainer: {},
-  btnContainer: {},
+  wordContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    flex: 7,
+  },
+  txtVoca: {
+    fontSize: 45,
+    margin: 8,
+  },
+  txtPronounce: {
+    fontFamily: 'wdpron',
+    fontSize: 23,
+    margin: 8,
+  },
+  txtMean: {
+    fontSize: 30,
+    margin: 8,
+  },
+  btnContainer: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   btnAdd: {},
   txtAdd: {},
 });
